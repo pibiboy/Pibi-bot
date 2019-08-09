f94e1a48ade56b3b398a6066eab3cd221142447b
pibiboy <tommygl2112@gmail.com>
2019-08-09 03:23:22 -0500

    cambio en el mensaje y respuesta

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const {Client} = require('discord.js');
 const client = new Client();
 
 client.on('ready', () => {
-    console.log('bot is ready');
+    console.log(`bot is ready as ${client.user.tag}`);
 });
 
 client.on('message', msg => {
-    if(msg.content === 'ping'){
-        msg.reply('Pong!');
+    if(msg.content === 'a ver'){
+        msg.reply('al cine prro');
     }
 });
 
