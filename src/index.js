f6eebb37d6ee5540cdd716f9caff9ef3cb67bf61
pibiboy <tommygl2112@gmail.com>
2019-08-10 04:47:43 -0500

    se agrego un punto y coma faltante

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
     console.log(`bot is ready as ${client.user.tag}`);
 });
 
-//a ver al cine prro
+//a ver al cine prro.
 client.on('message', msg => {
     if((msg.content.includes === 'a ver')||(msg.content.includes === 'A ver')){
         msg.channel.send('al cine prro');
@@ -18,10 +18,11 @@ client.on('message', msg => {
     }
 });*/
 
+//comando que inserta una foto de pibi.
 client.on('message', msg => {
     if(msg.content === '!pibi'){
         msg.channel.send('https://cdn.discordapp.com/attachments/423743123357958144/608569014926704660/unknown.png');
     }
-})
+});
 
 client.login('NjA5Mjg1NzAzNDk0NDAyMDU4.XU0fmQ.OOOVElMx2AfAY0Vcp3HESRZDUhQ');
