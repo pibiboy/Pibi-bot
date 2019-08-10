414c8564ee2d9b32551ecb382b2464bcf23cfead
pibiboy <tommygl2112@gmail.com>
2019-08-10 04:45:00 -0500

    cambios en 'al cine prro' y nuevo comando !pibi

--- src/index.js
+++ src/index.js
@@ -5,10 +5,23 @@ client.on('ready', () => {
     console.log(`bot is ready as ${client.user.tag}`);
 });
 
+//a ver al cine prro
 client.on('message', msg => {
-    if((msg.content === 'a ver')||(msg.content === 'A ver')){
+    if((msg.content.includes === 'a ver')||(msg.content.includes === 'A ver')){
         msg.channel.send('al cine prro');
     }
 });
+
+/*client.on('message', msg => {
+    if((msg.content === 'hola')||(msg.content === 'A ver')){
+        msg.channel.send(`hola ${msg.author}`);
+    }
+});*/
+
+client.on('message', msg => {
+    if(msg.content === '!pibi'){
+        msg.channel.send('https://cdn.discordapp.com/attachments/423743123357958144/608569014926704660/unknown.png');
+    }
+})
 
 client.login('NjA5Mjg1NzAzNDk0NDAyMDU4.XU0fmQ.OOOVElMx2AfAY0Vcp3HESRZDUhQ');
