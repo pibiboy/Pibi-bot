a653815d5a7c0816f1d410c9e97675f8634d8ef3
pibiboy <tommygl2112@gmail.com>
2019-08-10 19:54:31 -0500

    a oc11

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ client.on('message', msg => {
         msg.channel.send('https://cdn.discordapp.com/attachments/423743123357958144/608569014926704660/unknown.png');
     }
     //el bot contesta a oc cuando sigur escribe
-    if(msg.content.user.tag('Pibi#0120')){
+    if(msg.user.tag ==='Pibi#0120'){
         msg.channel.send('a oc');
     }
 
