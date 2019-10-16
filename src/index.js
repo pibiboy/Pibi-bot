19afd9bfa31cb4e619e05b6ee41a17437d11fcad
Tomas GL <53280884+pibiboy@users.noreply.github.com>
2019-10-16 18:27:51 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,26 +12,11 @@ client.on('message', msg => {
     if((msg.author.tag === 'Pibi#0120') && (msg.content.includes('si o no raza'))){
         msg.channel.send('si :3');
     }
-    //a ver al cine prro.
-    if(msg.content.includes('a ver')){
-        msg.channel.send('al cine prro');
+    
+    if(msg.content.includes('')){
+        msg.channel.send('');
     }
-    if(msg.content.includes('A ver')){
-        msg.channel.send('Al cine prro');
-    }
-    //esta prro
-    if(msg.content.includes('que juego')){
-        msg.channel.send('esta prro!')
-    }
-    if(msg.content.includes('Que juego')){
-        msg.channel.send('Esta prro!')
-    }
-    if(msg.content.includes('como')){
-        msg.channel.send('esta prro!')
-    }
-    if(msg.content.includes('Como')){
-        msg.channel.send('esta prro!')
-    }
+    
     //comando que inserta una foto de pibi.
     if(msg.content === '!pibi'){
         msg.channel.send('https://cdn.discordapp.com/attachments/423743123357958144/608569014926704660/unknown.png');
